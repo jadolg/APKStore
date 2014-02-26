2b326acab27b9e3da1707c39416385290027b498
Jorge Alberto Díaz Orozco <jaorozco@estudiantes.uci.cu>
2014-02-26 19:05:16 +0000

    con licencia

--- fillDB.py
+++ fillDB.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
+
+#  fillDB.py
+#
+#  Copyright 2014 Jorge Alberto Díaz Orozco <jaorozco@estudiantes.uci.cu>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+
 from zipfile import ZipFile
 from glob import glob as listDir
 from os import remove, mkdir, rename
